42fbf60c0a11e39e3bb88c8d61da7c8d96896fb9
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-04-15 18:23:40 +0530

    Add files via upload

---
+++ virtual.java
@@ -0,0 +1,33 @@
+class virtual
+{
+	public static void main(String arg[])
+	{
+      Base bp = new Base();
+      bp.fun();
+      bp.gun();
+	}
+}
+  class Base
+  {
+  	public  void fun()
+  	{
+  		System.out.println("Base fun");
+  	}
+
+  	public void gun()
+  	{
+  		System.out.println("Base gun");
+  	}
+  }
+   class Derived extends Base 
+   {
+   	public void fun()
+   	{
+   		System.out.println("Derived fun");
+   	}
+   	 public void gun()
+   	 {
+   	 	System.out.println("Derived gun");
+   	 }
+   }
+   
