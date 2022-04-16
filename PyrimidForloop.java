57861fb6fb77b26ee17e1cdee3edc6eddb04720f
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-04-16 14:01:59 +0530

    Add files via upload

---
+++ PyrimidForloop.java
@@ -0,0 +1,13 @@
+class PyrimidForloop{
+	public static void main(String arg[])
+	{
+	 for(int i=1;i<=5;i++)
+	 {
+	 	for(int j=1;j<=i;j++)
+	 	{
+         System.out.println("*");
+	 	}
+	 	System.out.println("\n");
+	 }
+	}
+}
