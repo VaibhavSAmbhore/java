f2ef198dec24e77eacc1efca835cef7c743ccea9
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-04-17 15:08:22 +0530

    Add files via upload

---
+++ Inputaccept.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Inputaccept{
+	public static void main(String arg[])
+	{
+	   Scanner sobj = new Scanner(System.in);
+	   System.out.println("enter a number");
+	   float  a= sobj.nextFloat();
+	   System.out.println("enter second numeber");
+	   Float b= sobj.nextFloat();
+	   Float c = a+b;
+	   System.out.println("add of two numbe="+c);
+
+	}
+}
