1f3bb63ae062a4dd79ee7276fc36433c35f24d2f
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-04-29 17:33:40 +0530

    Add files via upload

---
+++ Package.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Package
+{
+	public static void main(String arg[])
+	{
+	  Scanner sobj = new Scanner(System.in);
+	  System.out.println("enter a value");
+	  int no = sobj.nextInt();
+	  System.out.println("enter string is"+ no);
+	}
+}
