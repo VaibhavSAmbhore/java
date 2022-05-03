bbeaab6f4c275b404d62204d710ac0d327eba31d
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-03 13:00:09 +0530

    Add files via upload

---
+++ javaException.java
@@ -0,0 +1,17 @@
+
+
+public class javaException
+{
+  public static void main (String arg[])
+  {
+   try
+  {
+    int data= 100/0;
+  }catch(ArithmeticException e)
+  {
+    System.out.println(e);
+  }
+  System.out.println("rest of the code...");
+  }
+  
+}
