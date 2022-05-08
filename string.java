42b8109a7564f259430d76f344b4c36c55376c16
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-08 17:20:23 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,13 @@
+class string
+{
+    public static void main(String arg[])
+    {
+      String s1="java";
+      char ch []= {'s','t','r','i','n','g'};
+      String s2= new String(ch);
+      String s3= new String("String");
+      System.out.println(s1);
+      System.out.println(s2);
+      System.out.println(s3);
+    }
+}
