bd1868e27bbe3b8af9640c2ebe62bb7a7bdb0c72
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-05-09 17:09:43 +0530

    Add files via upload

---
+++ ReplayString.java
@@ -0,0 +1,10 @@
+class ReplayString
+{
+    public static void main(String arg[])
+    {
+        String S1="java is best programing language.java is platfrom independance.this is best way";
+        String ReplayString = S1.replace("java","kiva");
+        System.out.println(ReplayString);
+
+    }
+}
