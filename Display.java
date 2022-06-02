7d8dfaa9609fe771107b3281e25125f6c48f3346
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-06-02 12:32:33 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Display
+{
+	public static void main(String arg[])
+	{ 
+		Scanner sc= new Scanner(System.in);                                        /* int nextInt();float nextFloat();String nextLine();double nextDouble();*/
+		System.out.println("enter a number");
+		double i=sc.nextDouble();
+		//String i=sc.nextLine();
+		System.out.println("enter is:"+i);
+		
+		//star sboj = new star();
+  	/*	System.out.println("enter a second number");
+   float f =sc.nextFloat();
+    float c;
+	 c = i + f;
+	 System.out.println("Addition of two number is "+c);*/
+	 /* for(int iCnt=0;iCnt<=i;iCnt++)
+    {
+			System.out.println("*");
+		}
+  	  
+    
+   }*/
+   	/* public class star(i)
+	   {
+	   for(int iCnt=0;iCnt<=i;iCnt++)
+     {
+			System.out.println("*");
+		 }
+   
+	  */
+	}	 	
+}
