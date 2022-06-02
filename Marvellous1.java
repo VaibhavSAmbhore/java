fca694960e847d35d9a409a500fcc6c984a5425c
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-06-03 00:19:07 +0530

    Add files via upload

---
+++ Marvellous1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Marvellous1
+{
+  public static void main(String arg[])
+  {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a number");
+    int ino = sc.nextInt();
+    for(int i=1;i<=ino; i++)
+    {
+      System.out.println("marvellous");
+    }
+  }
+}
