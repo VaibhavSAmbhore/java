9e5c317ed3c32b52a8aa21b34ae05056fc6d064d
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-06-05 01:49:36 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,15 @@
+// problem on number
+
+class Number
+{
+  public static void main(String arg[])
+  {
+    int ino =10;
+    int isum=0;
+    for(int i=1;i<=10;i++)
+    {
+      isum = isum + i;
+    }
+    System.out.println(isum);
+  }
+}
