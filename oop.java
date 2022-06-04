1a1c7bab788a197d49eaa44de386490aab2c88b8
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-06-05 01:24:04 +0530

    Add files via upload

---
+++ oop.java
@@ -0,0 +1,35 @@
+
+
+class oop
+{
+  
+  
+  public static void main (String arg[])
+  {
+  // oop obj = new oop(ivalue);
+  //  int iRet = obj.oop();
+  //  System.out.println("the digit:"+iRet);
+  
+  int ino=10;
+  int iDigit;
+   
+     while (ino != 0)
+   {
+      iDigit = ino % 10;
+      System.out.println(iDigit);
+      ino = ino/10;
+   }
+   //  return oop;
+   
+  
+  }
+
+
+/*
+ public static void main (String arg[])
+  {
+    oop obj = new oop(ivalue);
+    int iRet = obj.oop();
+    System.out.println("the digit:"+iRet);
+  }*/
+}
