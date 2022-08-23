39ba2c0c1a7cae9e6a2b9b76c18dbb7662fd010f
Vaibhav Sudhakar Ambhore <93433830+VaibhavSAmbhore@users.noreply.github.com>
2022-08-23 16:16:28 +0530

    Add files via upload

---
+++ Emp1.java
@@ -0,0 +1,21 @@
+class Emp1
+{
+  public static void main(String arg[])
+  
+  class Base{
+    public float salary=40000;
+    System.out.println("salary is "+40000);
+  }
+
+  class Derived extends Base
+  {
+    Derived dobj = new Derived();
+    int bonus = 10000;
+    n
+   /* System.out.println("programmer salary is:"+dobj.salary);
+    System.out.println("programmer salary is "+dobj.bonus);*/
+    dobj.Base();
+
+
+  }
+}
